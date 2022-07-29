9adf7a05a9aee94c5c6fc640a3ceceba449eec30
jamespaologarcia <james.paolo.p.garcia@gmail.com>
2022-07-28 21:45:21 -0700

    Added currency converter

--- includes/engine.js
+++ includes/engine.js
@@ -1,20 +1,9 @@
 $(document).ready(function(){
     loadData();
     closeNav();
-    reopulateCart();
-    $("#mySidenav").show();
-   openNav();
-
-
-   exampleFetch();
-   
+    populateCart();
+   openNav(); 
 });
-
-
-async function exampleFetch() {
-   
-}
-
 
 
 $('.grid').masonry({
@@ -56,14 +45,14 @@ function addToCart(id){
         indexFound =list.length - 1;
     }
     set_cookie("cartList", list);
-  reopulateCart();
+  populateCart();
 
 }
 function removeFromCart(id){
     let list = get_cookie("cartList");
     list.splice(id, 1);
     set_cookie("cartList", list);
-    reopulateCart();
+    populateCart();
 }
 function populateStore(apiURL){
     let storeProducts = "";
@@ -90,54 +79,66 @@ function populateStore(apiURL){
 }
 function emptyCart(){
     set_cookie("cartList", []);
-    reopulateCart();
+    populateCart();
 }
-async function reopulateCart(){
-    $("#cartContainer").empty();
-    $("#totalAmount").empty();
-    let cartList = get_cookie("cartList");
-    cartHeaders();
-    if(cartList.length > 0 && cartList !== undefined){
-        var totalPrice = 0;
-        for(let i = 0; i< cartList.length; i++){
-            let response = await fetch(`https://fakestoreapi.com/products/${cartList[i][0]}`);
-            let cartItem = await response.json();
-            $('#cartContainer').append(`<div class="row" id="row${cartList[i][0]}">
-            <div class="col-lg-2 shopping-list">
-                <button type="button" class="btn btn-danger" onclick="removeFromCart(${i})"><span class="material-symbols-outlined">
-                close
-                </span></button>
-                
-            </div>
-            <div class="col-lg-3  shopping-list">
-                ${cartItem['title']}
-            </div>
-            <div class="col-lg-2  shopping-list">
-                ${cartList[i][1]}
-            </div>
-            <div class="col-lg-2  shopping-list">
-                ${cartItem['price']}
-            </div>
-            <div class="col-lg-3  shopping-list">
-                ${cartItem['price'] * cartList[i][1]}
-            </div>
-            <hr class ="shopping-list">
-            </div>`);  
-            totalPrice += cartItem['price'] * cartList[i][1];     
-            if(i == cartList.length - 1){
-                get_total(totalPrice.toFixed(2))
-                addDivFooter();
-                
-            }                   
+
+async function populateCart(currency){
+    let promisedCurrency = getCurrency();
+    if(currency === undefined){
+        promisedCurrency.then(function(xa) {
+            populateCart(xa);
+         });
+    }
+    else {
+        $("#cartContainer").empty();
+        $("#totalAmount").empty();
+        let cartList = get_cookie("cartList");
+        cartHeaders();
+        if(cartList.length > 0 && cartList !== undefined){
+            var totalPrice = 0;
+            for(let i = 0; i< cartList.length; i++){
+                let response = await fetch(`https://fakestoreapi.com/products/${cartList[i][0]}`);
+                let cartItem = await response.json();
+                let sum = (cartItem['price'] * cartList[i][1]) * currency;
+                let itemPrice = (cartItem['price'] * currency).toFixed(2);
+                $('#cartContainer').append(`<div class="row" id="row${cartList[i][0]}">
+                <div class="col-lg-2 shopping-list">
+                    <button type="button" class="btn btn-danger" onclick="removeFromCart(${i})"><span class="material-symbols-outlined">
+                    close
+                    </span></button>
+                    
+                </div>
+                <div class="col-lg-3  shopping-list">
+                    ${cartItem['title']}
+                </div>
+                <div class="col-lg-2  shopping-list">
+                    ${cartList[i][1]}
+                </div>
+                <div class="col-lg-2  shopping-list">
+                    ${itemPrice}
+                </div>
+                <div class="col-lg-3  shopping-list">
+                    ${sum.toFixed(2)}
+                </div>
+                <hr class ="shopping-list">
+                </div>`);  
+                totalPrice += sum;     
+                if(i == cartList.length - 1){
+                    get_total(totalPrice.toFixed(2))
+                    addDivFooter();
+                    
+                }                   
+            }
+        }
+        else{
+            $('#cartContainer').append(`<div class="row"> 
+                <div class="col-lg-12  shopping-list">
+                  <center> No items in cart </center>
+                </div>
+            </div>`);
         }
     }
-    else{
-        $('#cartContainer').append(`<div class="row"> 
-            <div class="col-lg-12  shopping-list">
-              <center> No items in cart </center>
-            </div>
-        </div>`);
-    }
+    
 }
 function addDivFooter(){
     $('#totalAmount').append(`<div class="row shopping-list">
@@ -191,4 +192,34 @@ function cartHeaders(){
     </div>
     <hr>
     </div>`);   
+}
+
+$().change({
+   
+});
+
+
+$("#selectCurrency").on('change', async function (e) {
+    populateCart();
+});
+function getCurrency(){
+    let selectValue = $("#selectCurrency :selected").val(); 
+    let data = fetch("https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/cad.json").
+    then(response => response.json()).
+        then((json) => { 
+            return json["cad"][selectValue];
+        });
+    return data;
+}
+
+class currencyEngine {
+    constructor(){
+        this._currency = 1;
+    }
+     get currentCurrency(){
+        return this._currency;
+    }    
+    set currentCurrency(currency){
+        this._currency = currency;
+    }
 }
