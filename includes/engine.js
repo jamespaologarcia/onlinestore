df180278e2d03e06092456ec1038a53154426665
jamespaologarcia <james.paolo.p.garcia@gmail.com>
2022-07-30 05:07:28 -0700

    Added currency conversion to shop items and moved currency selector to main div

--- includes/engine.js
+++ includes/engine.js
@@ -1,9 +1,11 @@
+
 $(document).ready(function(){
     loadData();
     closeNav();
     populateCart();
     openNav(); 
     set_cookie("cartList", {});
+    
 });
 $('.grid').masonry({
     // options
@@ -14,6 +16,7 @@ function loadData(){
     $("#demo").empty();
     let apiURL = "https://fakestoreapi.com/products";
     let storeProducts = populateStore(apiURL);
+    $(".grid").empty();
     $('.grid').append(storeProducts);
 }
 function openNav() {
@@ -36,7 +39,6 @@ function addToCart(id){
     else{
     list[id]++;
     }
-    console.log(list);
     set_cookie("cartList", list);
     populateCart();
 }
@@ -48,27 +50,28 @@ function removeFromCart(id){
     set_cookie("cartList", list);
     populateCart();
 }
-function populateStore(apiURL){
+function populateStore(apiURL, currency){
     let storeProducts = "";
-    fetch(apiURL).
-    then(response => response.json()).
-        then((json) => {
-            json.forEach((data, index) => {
-                $(".grid").append(`                    
-                    <div class="grid-item">
-                        <div class="card">
-                        <img src="${data.image}" height="auto" width="200px"></img>
-                            <div class="card-body">
-                                <h5 class="card-title">${data.title}</h5>
-                                <p class="card-text">${data.description}</p>
-                                <button class="btn btn-success add-to-cart-button" id="${data.id}" onclick="addToCart(${data.id}); openNav();">
-                                Add to Cart</button>
-                            </div>
-                        </div>
-                </div>`);
-            });
-        }); 
-    return storeProducts;
+    let renderer = new shopBuilder();
+    let promisedCurrency = renderer.getCurrency();
+    if(currency === undefined){
+        promisedCurrency.then(function(xa) {
+            populateStore(apiURL, xa)
+         });
+    }
+    else {
+        fetch(apiURL).
+        then(response => response.json()).
+            then((json) => {
+                json.forEach((data) => {
+                    let price = data.price * currency;
+                    let itemEntry = renderer.renderShopItemEntry(data,price);
+                    $(".grid").append(itemEntry);
+                });
+            }); 
+        return storeProducts;
+    }
+    
 }
 function emptyCart(){
     set_cookie("cartList", {});
@@ -123,10 +126,10 @@ function cartHeaders(){
     <div class="col-lg-3 ">
     Item    
     </div>
-    <div class="col-lg-2 ">
+    <div class="col-lg-1 ">
     Qty
     </div>
-    <div class="col-lg-2 ">
+    <div class="col-lg-3 ">
     Price
     </div>
     <div class="col-lg-3 ">
@@ -135,16 +138,20 @@ function cartHeaders(){
     <hr>
     </div>`);   
 }
-$("#selectCurrency").on('change', async function (e) {
+$("#selectCurrency").on('change', function (e) {
     $("#cartContainer").empty();
     $("#totalAmount").empty();
     populateCart();
+    loadData();
 });
 class shopBuilder{
     constructor(){
-
+        this._selectCurrency = $("#selectCurrency :selected").val(); 
+        this._currency = {"cad": "$", "usd": "US$", "gbp": "£"};
+        this._currencySymbol = this._currency[this._selectCurrency];
     }
     buildCart(item, cartList, currency){
+        
         let sum = cartList[item['id']] * item['price'] * currency;
         let itemPrice = item['price'] * currency;
         let cartRow = `<div class="row" id="row${item['id']}">
@@ -156,18 +163,34 @@ class shopBuilder{
         <div class="col-lg-3  shopping-list">
             ${item['title']}
         </div>
-        <div class="col-lg-2  shopping-list">
+        <div class="col-lg-1  shopping-list">
             ${cartList[item['id']]}
         </div>
-        <div class="col-lg-2  shopping-list">
-            ${itemPrice.toFixed(2)}
+        <div class="col-lg-3  shopping-list">
+        ${this._currencySymbol} ${itemPrice.toFixed(2)}
         </div>
         <div class="col-lg-3  shopping-list">
-            ${sum.toFixed(2)}
+           ${this._currencySymbol} ${sum.toFixed(2)}
         </div>
         <hr class ="shopping-list">
         </div>`; 
         return cartRow;
+    }
+    renderShopItemEntry(data,price){
+        console.log(this._currencySymbol);
+        let itemEntry = `<div class="grid-item">
+            <div class="card">
+            <img src="${data.image}" height="auto" width="200px"></img>
+                <div class="card-body">
+                    <h5 class="card-title">${data.title}</h5>
+                    <p class="card-text">${data.description}</p>
+                    <p class="card-text">${this._currencySymbol}  ${price.toFixed(2)}</p>
+                    <button class="btn btn-success add-to-cart-button" id="${data.id}" onclick="addToCart(${data.id}); openNav();">
+                    Add to Cart</button>
+                </div>
+            </div>
+        </div>`;
+        return itemEntry;
     }
     getCurrency(){
         let selectValue = $("#selectCurrency :selected").val(); 
@@ -208,7 +231,7 @@ class shopBuilder{
         <div class="col-lg-2 shopping-list ">
         </div>
         <div class="col-lg-3 shopping-list" id="totalPrice">
-        <label id="totalPrice">${totalPrice}
+        <label id="totalPrice"> ${this._currencySymbol} ${totalPrice}
         </div>
         <hr class = "shopping-list">
         </div>`);
